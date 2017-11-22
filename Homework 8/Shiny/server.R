7fa9c52eda1f4166a531c6763bc6f7d6ecde342e
vanflad <vanessa40_40@hotmail.com>
2017-11-21 21:41:40 -0800

    updated files

--- Homework 8/Shiny/server.R
+++ Homework 8/Shiny/server.R
@@ -3,114 +3,100 @@ server <- function(input, output){
 	library(readr)
 	library(tidyverse)
 	library(stringr)
-	fish <- readr::read_csv(file="fish.csv")
-	fish$date <- as.character(fish$date)
-	fish <- mutate(fish, year=str_trunc(date, 4, ellipsis = ""))
-	fish$date <- as.Date(fish$date)
+	library(DT)
+	library(shiny)
+	library(ggvis)
+	library(cowplot)
+	library(dplyr)
+	#load any libraries used within this file, maybe even extra just in case?
+	fishy <- readr::read_csv(file="fish.csv")
+	#read in file
+	fishy$date <- as.character(fishy$date)
+	#change to character to extract year from the date
+	fishy <- mutate(fishy, year=str_trunc(date, 4, ellipsis = ""))
+	#make a column for year
+	fishy <- filter(fishy, is.na(lat) != TRUE )
+	#remove useless rows that are full of NAs but still have site and date
+	fishy$date <- as.Date(fishy$date)
+	#change date back into date format for proper usage
 	
-	colnames(fish)[5:7] <- str_trunc(colnames(fish[5:7]), 2, ellipsis = "")
-	fish <- gather(fish, Species, Number, so:pi, na.rm = TRUE)
+	colnames(fishy) <- c("Date", "Site", "Latitude", "Longitude", "Sockeye", "Chum", "Pink", "Year")
+	#change column names so things are easier and look nicer later on!
+	fishy <- gather(fishy, Species, Number, Sockeye:Pink, na.rm = TRUE)
+	#change to long format with columns for species and for the number of samples
 	
-	fish$site.ID <- as.factor(fish$site.ID)
-	 
-	output$Hist_2015Fish <- renderPlot({
-		if(input$filterIn==TRUE){
-		if(input$yearIn=="2015"){
-		fish %>%
-			filter(date >= input$dateIn5[1],
-						 date <= input$dateIn5[2],
-						 site.ID %in% input$stnIn,
-						 year=="2015") %>% 
-			ggplot(aes(x=date, y=Number))+
-			geom_bar(aes(fill=Species), stat = "identity", position = "dodge")+
-			theme_classic()+
-			labs(title="Juvenile Salmon Species Collected 2015", x="Date", y="Number of Salmon")+
-			scale_fill_discrete(labels=c("Chum", "Pink", "Sockeye"))
-		}} else{
-			fish %>%
-				filter(date >= input$dateIn5[1],
-							 date <= input$dateIn5[2],
-							 site.ID %in% input$stnIn,
-							 year=="2015") %>% 
-				ggplot(aes(x=date, y=Number))+
-				geom_bar(aes(fill=Species), stat = "identity", position = "dodge")+
-				theme_classic()+
-				labs(title="Juvenile Salmon Species Collected 2015", x="Date", y="Number of Salmon")+
-				scale_fill_discrete(labels=c("Chum", "Pink", "Sockeye"))
-	}})
+	fishy$Number <- as.numeric(fishy$Number)
+	#make sure number is in the right format to prevent errors!
 	
-	output$Hist_2016Fish <- renderPlot({
-		if(input$filterIn==TRUE){
-		if(input$yearIn=="2016"){
-		fish %>%
-			filter(date >= input$dateIn6[1],
-						 date <= input$dateIn6[2],
-						 site.ID %in% input$stnIn,
-						 year=="2016") %>% 
-			ggplot(aes(x=date, y=Number))+
-			geom_bar(aes(fill=Species), stat = "identity", position = "dodge")+
-			theme_classic()+
-			labs(title="Juvenile Salmon Species Collected 2016", x="Date", y="Number of Salmon")+
-			scale_fill_discrete(labels=c("Chum", "Pink", "Sockeye"))
-		}} else{
-			fish %>%
-				filter(date >= input$dateIn6[1],
-							 date <= input$dateIn6[2],
-							 site.ID %in% input$stnIn,
-							 year=="2016") %>% 
-				ggplot(aes(x=date, y=Number))+
-				geom_bar(aes(fill=Species), stat = "identity", position = "dodge")+
-				theme_classic()+
-				labs(title="Juvenile Salmon Species Collected 2016", x="Date", y="Number of Salmon")+
-				scale_fill_discrete(labels=c("Chum", "Pink", "Sockeye"))
-	}})
-	
-	output$table_head <- renderTable({
-		fish %>%
-			filter(date >= input$dateIn5[1],
-						 date <= input$dateIn6[2],
-						 site.ID %in% input$stnIn,
-						 year==input$yearIn) %>% 
-			head()
+	fishy$Site <- as.factor(fishy$Site)
+	#make sure site is a factor so it works properly as well
+
+	fish <- reactive({
+		if(input$yearIn == "2015"){
+			filter(fishy, Site %in% input$stnIn, Date >= input$dateIn5[1],
+						 Date <= input$dateIn5[2], Number != 0,
+						 Year=="2015")
+		} else{
+			filter(fishy, Site %in% input$stnIn, Date >= input$dateIn6[1],
+								 Date <= input$dateIn6[2], Number != 0,
+								 Year=="2016")
+			}
 	})
+	#create a reactive dataframe where it returns inputs chosen by the user
+	#it filters by date range relevant to year, site and year itself
+	#also drops any rows with 0 for number of fish since not relevant
+	
+	output$Hist_Fish <- renderPlot({
+		fish() %>% 
+			ggplot(aes(x=Date, y=Number))+ 
+			geom_bar(aes(fill=Species), stat = "identity", position = "dodge")+
+				labs(title="Juvenile Salmon Species Collected", y="Number of Salmon")
+	})
+	#graph of the number of salmon collected, by site, by date, by species, by year!
+	
+	pink <- reactive({
+		fish() %>%
+			filter(Species=="Pink") %>%
+			summarise(sum(Number)) %>%
+			as.character()
+	})
+	#create reactive dataframe that further filters previous reactive dataframe
+	#so that it will return the number of pink salmon samples the user has selected
+	
+	chum <- reactive({
+		fish() %>%
+			filter(Species=="Chum") %>%
+			summarise(sum(Number)) %>%
+			as.character()
+	})
+	#repeat process to find number of chum salmon samples currently selected
+	
+	sock <- reactive({
+		fish() %>%
+			filter(Species=="Sockeye") %>%
+			summarise(sum(Number)) %>%
+			as.character()
+	})
+	#repeat once more so that it also prints out number of sockeye salmon samples
 	
 	output$textresults <- renderText({
-		paste("We found",
-		fish %>%
-			filter(date >= input$dateIn5[1],
-						 date <= input$dateIn6[2],
-						 site.ID %in% input$stnIn,
-						 Species=="pi",
-						 year==input$yearIn) %>%
-			summarise(sum(Number)),
-		"pink,",
-		fish %>%
-			filter(date >= input$dateIn5[1],
-						 date <= input$dateIn6[2],
-						 site.ID %in% input$stnIn,
-						 Species=="cu",
-						 year==input$yearIn) %>%
-			summarise(sum(Number)),
-		"chum and",
-		fish %>%
-			filter(date >= input$dateIn5[1],
-						 date <= input$dateIn6[2],
-						 site.ID %in% input$stnIn,
-						 Species=="so",
-						 year==input$yearIn) %>%
-			summarise(sum(Number)),
-		"sockeye samples for you!")
+		paste("We found", pink(), "Pink,", chum(), "Chum and", sock(), "Sockeye samples for you!")
 		})
+	#combine into a text output that says how many fish are being viewed
 	
 	output$downloadID <- downloadHandler(
 		filename = function(){
 			paste("salmon-data-", Sys.Date(), ".csv", sep="")
 		},
 		content = function(file){
-			write_csv(fish %>%
-									filter(date >= input$dateIn5[1],
-												 date <= input$dateIn6[2],
-												 site.ID %in% input$stnIn,
-												 year==input$yearIn), file)
+			write_csv(fish(), file)
 		})
-	}
+	#gives an option to download the current datatable, with recent date attached
+	
+	output$table <- renderDataTable({
+			fish() %>% 
+				spread(Species, Number, fill = 0) %>%
+				select(Year, Date, Site, Pink, Chum, Sockeye)
+	})
+	#interactive table that displays the detailed numbers of fish per site per date
+}
--- Homework 8/Shiny/ui.R
+++ Homework 8/Shiny/ui.R
@@ -1,30 +1,38 @@
 ui <- fluidPage(
-	#Application title
 	titlePanel("Hakai Salmon Sampling"),
+	#title
 	
 	sidebarPanel("Juvenile salmon collected in 2015-2016 for the Salmon Early Marine Survival Program,
 							 by species and site to select for my thesis research on salmon diets.",
 							 br(),
+							 #subtitle and insert a break
 							 img(src="map.png", width = "100%"),
 							 br(), br(),
+							 #add an image of map and add breaks
 							 dateRangeInput("dateIn5", "Dates of Sampling for 2015",
 							 						min = "2015-05-12", max = "2015-07-14",
 							 						start = "2015-06-20", end = "2015-06-30"),
+							 #give options for 2015 dates
 							 dateRangeInput("dateIn6", "Dates of Sampling for 2016",
 							 							 min = "2015-05-06", max = "2016-07-06",
 							 							 start = "2016-06-15", end = "2016-06-27"),
-							 checkboxInput("filterIn", "Filter by year?"),
-							 conditionalPanel("input.filterIn==true", selectInput("yearIn", "Year", choices = c("2015", "2016"), selected = "2015")),
+							 #give options for 2016 dates
+							 radioButtons("yearIn", "Year", c("2015", "2016"), "2015"),
+							 #choose which year to look at
 							 checkboxGroupInput("stnIn", "Which stations?",
 							 									 choices=c("D06", "D07", "D08", "D09", "D10", "D11", "J02",
 							 									 					"J04", "J05", "J06", "J07", "J08", "J09"),
 							 									 selected = c("D07", "D09", "J05", "J07", "J09"))),
+	#the relevant stations to investigate, can choose multiple stations!
 	
 	mainPanel(textOutput("textresults"),
+						#prints out how many species of each fish were sampled
 						downloadButton("downloadID", label = "Download"),
+						#gives an option to download once decided on important stations/dates!
 						br(), br(),
-						plotOutput("Hist_2015Fish"),
-						plotOutput("Hist_2016Fish"),
+						plotOutput("Hist_Fish"),
+						#gives a graph to display sampling data
 						br(), br(),
-						tableOutput("table_head"))
+						DT::dataTableOutput("table"))
+	#prints out an interactive table of the selected sampling data
 )
