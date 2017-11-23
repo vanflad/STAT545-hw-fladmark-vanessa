1d53ec86592c34d5147bded0a41c5bed1188af6b
vanflad <vanessa40_40@hotmail.com>
2017-11-23 12:28:03 -0800

    update app for research meeting

--- Homework 8/Shiny/ui.R
+++ Homework 8/Shiny/ui.R
@@ -1,4 +1,5 @@
 ui <- fluidPage(
+	
 	titlePanel("Hakai Salmon Sampling"),
 	#title
 	
@@ -14,15 +15,17 @@ ui <- fluidPage(
 							 						start = "2015-06-20", end = "2015-06-30"),
 							 #give options for 2015 dates
 							 dateRangeInput("dateIn6", "Dates of Sampling for 2016",
-							 							 min = "2015-05-06", max = "2016-07-06",
+							 							 min = "2016-05-06", max = "2016-07-06",
 							 							 start = "2016-06-15", end = "2016-06-27"),
 							 #give options for 2016 dates
 							 radioButtons("yearIn", "Year", c("2015", "2016"), "2015"),
 							 #choose which year to look at
 							 checkboxGroupInput("stnIn", "Which stations?",
-							 									 choices=c("D06", "D07", "D08", "D09", "D10", "D11", "J02",
-							 									 					"J04", "J05", "J06", "J07", "J08", "J09"),
-							 									 selected = c("D07", "D09", "J05", "J07", "J09"))),
+							 									 choices=c("D01", "D02", "D03", "D04", "D05", "D06", "D07", "D08", "D09", "D10", "D11", "D20", "D21", "D22",
+							 									 					"D23", "D24", "D25", "D27", "D28", "J01", "J02", "J03", "J04", "J05", "J06", "J07", "J08", "J09",
+							 									 					"J10"),
+							 									 selected = c("D06", "D07", "D08", "D09", "D10", "D11", "J02",
+							 									 						 "J04", "J05", "J06", "J07", "J08", "J09"))),
 	#the relevant stations to investigate, can choose multiple stations!
 	
 	mainPanel(textOutput("textresults"),
@@ -35,4 +38,6 @@ ui <- fluidPage(
 						br(), br(),
 						DT::dataTableOutput("table"))
 	#prints out an interactive table of the selected sampling data
-)
+	)
+	
+	
