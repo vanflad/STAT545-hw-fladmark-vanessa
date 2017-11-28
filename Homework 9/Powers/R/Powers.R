3a898d13573e30cab6cd2042c0d0601e6db532d3
vanflad <vanessa40_40@hotmail.com>
2017-11-28 15:06:04 -0800

    final answer to problem hopefully...

--- Homework 9/Powers/R/Powers.R
+++ Homework 9/Powers/R/Powers.R
@@ -1,16 +1,16 @@
 #' Powers
 #'
 #' @name Powers
-#' @docType package
+#' @docType Package
 #'
 #' @param x The vector to be transformed (squared/cubed/etc.)
-#' 
+#'
 #' @param a The power to be calculated to, for the transformation
 #'
 #' @return A vector \code{x} that is raised to the power of \code{a}.
 #'
 #' @details
-#' 
+#'
 #' This is a simple package that calculates powers, which is a number multiplied by itself a certain amount of times.
 #'
 #' The functions include:
@@ -20,7 +20,7 @@
 #'      \item cube (for \code{cube})
 #'      \item reciprocal/inverse (for \code{reciprocal})
 #' }
-#' 
+#'
 #' Each function also returns an informative error when a non-numeric input is given.
 #'
 #' @examples
@@ -28,5 +28,4 @@
 #' square(1:10)
 #' cube(-5)
 #' reciprocal(-2, 3)
-#' @export
 NULL
